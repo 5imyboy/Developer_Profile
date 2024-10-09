baf3c0731c83f3c2d77b8892a4d99038ffc73dfd
Simon Yong <simonyong05@gmail.com>
2024-10-09 15:49:43 -0500

    tutorial: render 3D model on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import RenderModel from "./components/RenderModel";
+import Book from "./components/models/Book";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <div className="w-full h-screen">
+        {
+          /* Navigation and 3D model */
+          <RenderModel>
+            <Book />
+          </RenderModel>
+        }
+      </div>
     </main>
   );
 }
