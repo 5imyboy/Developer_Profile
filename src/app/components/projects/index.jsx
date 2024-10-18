7f425e928977ba2b264842a1780acb74f9c815f8
Simon Yong <simonyong05@gmail.com>
2024-10-18 16:17:55 -0500

    tutorial: make projects page mobile responsive

--- src/app/components/projects/index.jsx
+++ src/app/components/projects/index.jsx
@@ -4,7 +4,7 @@ const ProjectList = ({projects}) => {
   return (
     <div>
       {
-        <div className='w-full m-w-fxl px-16 space-y-8 flex flex-col items-center'>
+        <div className='w-full max-w-auto xl:max-w-4xl px-4 lg:px-16 mx-auto space-y-6 md:space-y-8 flex flex-col items-center'>
           {projects.map((project, index) => {
             return <ProjectLayout key={index} {...project} />
            })}
