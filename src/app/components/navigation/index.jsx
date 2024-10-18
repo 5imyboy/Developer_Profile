aa840a94bf423bc562178f618e9d61399e955369
Simon Yong <simonyong05@gmail.com>
2024-10-18 14:26:47 -0500

    tutorial: adjust navigation circle radius based on width

--- src/app/components/navigation/index.jsx
+++ src/app/components/navigation/index.jsx
@@ -2,9 +2,15 @@
 import { BtnList } from '@/app/data'
 import React from 'react'
 import NavButton from './NavButton'
+import useScreenSize from '../hooks/useScreenSize'
 
 const Navigation = () => {
     const angleIncrement = 2*Math.PI / BtnList.length
+    const size = useScreenSize()
+
+    const isLarge = (size >= 1024);
+    const isMedium = (size >= 768);
+
     return (
         // For some reason, width is not centered at 100%
         <div className='w-full fixed h-screen flex items-center justify-center'>
@@ -12,7 +18,7 @@ const Navigation = () => {
                 {
                     BtnList.map((btn, index) => {
                         const btnAngle = index * angleIncrement
-                        const radius = 'calc(20vw - 1rem)'
+                        const radius = isLarge ? 'calc(20vw - 1rem)' : (isMedium ? 'calc(30vw - 1rem)' : 'calc(40vw - 1rem')
                         const x = `calc(${radius}*${Math.cos(btnAngle)})`
                         const y = `calc(${radius}*${Math.sin(btnAngle)})`
                         return <NavButton key={btn.label} x={x} y={y} {...btn} />
