b278e659c981fdd9fc311a023f7b543121bfdf7f
Simon Yong <simonyong05@gmail.com>
2024-11-15 16:51:07 -0600

    add tital to hompage

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
       <Image priority sizes="100vw" src={bg} alt="background-image" fill className="w-full h-full object-cover object-center opacity-25"/>
-      <div className="w-full h-screen">
+      <div className="w-full h-screen flex flex-col">
+        <div className="w-full fixed">
+          <p className="text-center text-foreground text-4xl p-4">
+            Simon's Developer Website
+          </p>
+        </div>
         { /* Navigation and 3D model */ }
           <Navigation />
           <RenderModel>
